6a4edcc9d73e3a8dc94bec210aa9cef253a0de5e
FlautistaMacabro <vinicius.pelegrineli@unesp.br>
2021-05-22 21:51:57 -0300

    Adicionando ícones e mostrando como mudar o conteúdo a partir deles

--- js/chargePages.js
+++ js/chargePages.js
@@ -1,4 +1,18 @@
-window.addEventListener("load", function CallHome(){
+function CallHome() {
     document.getElementById("content").innerHTML = '<object type="text/html" data="home.html"></object>';
-})
+}
+
+window.addEventListener("load", CallHome())
+
+function changeContent(valor) {
+    switch (valor) {
+        case "home": CallHome();
+            break;
+        case "user": document.getElementById("content").innerHTML = '<object type="text/html" data="confirmation.html"></object>';
+            break;
+        case "cart": document.getElementById("content").innerHTML = '<object type="text/html" data="confirmation.html"></object>';
+            break;
+        default: alert("Erro ao redirecionar a página! Por favor, notifique o administrador");
+    }
+}
 
--- js/fixedScroll.js
+++ js/fixedScroll.js
@@ -2,6 +2,7 @@ document.addEventListener('scroll', function AtualizarHeight(params) {
     let srllPos = window.scrollY*0.2;
     let divfixed = document.getElementById("fixed");
     let header = document.getElementsByTagName("header")[0];
+    let iconsheader = document.getElementById("diviconsheader");
     let title = document.getElementById("title");
     let divheader = document.getElementById("divheader");
     let nav = document.getElementsByTagName("nav")[0];
@@ -12,6 +13,8 @@ document.addEventListener('scroll', function AtualizarHeight(params) {
         AtualizarHeight.divheader = divheader.offsetHeight;
         AtualizarHeight.nav = nav.offsetHeight;
         AtualizarHeight.title = parseInt(window.getComputedStyle(title, null).getPropertyValue('font-size'));
+        AtualizarHeight.iconsheader = parseInt(window.getComputedStyle(iconsheader, null).getPropertyValue('margin-top'));
+            
     }
 
     if (srllPos < 40){
@@ -20,5 +23,6 @@ document.addEventListener('scroll', function AtualizarHeight(params) {
         nav.style.setProperty('height', `${AtualizarHeight.nav - (srllPos*0.3)}px`);
         title.style.setProperty('font-size', `${AtualizarHeight.title - (srllPos*0.3)}px`);
         divheader.style.setProperty('height', `${AtualizarHeight.divheader - srllPos}px`);
+        iconsheader.style.setProperty('margin-top', `${AtualizarHeight.iconsheader - (srllPos*0.4)}px`)
     }
 })
