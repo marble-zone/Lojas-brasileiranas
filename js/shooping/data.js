69782d40b2753b2ea9ad27c40bdb4edc7df3d386
Prado27 <gp.araujo@unesp.br>
2021-06-05 22:10:18 -0300

    Mais produtos e filtros

--- js/shooping/data.js
+++ js/shooping/data.js
@@ -3,7 +3,7 @@ let urlBase = "images/products/"
     C: cosméticos
     A: alimentos e bebidas
     M: mobília
-    E: eletrodonesticos
+    E: eletrodomesticos
     I: informática
     R: roupas
 */
@@ -24,4 +24,95 @@ var PRODUTOS = [{
     "preco": 199.99,
     "img": `${urlBase}cama.png`,
     "cat": 'M'
-}];
+},
+{
+    "nome": "Armário",
+    "preco": 700,
+    "img": `${urlBase}armario.png`,
+    "cat": 'M'
+},
+{
+    "nome": "Sapato",
+    "preco": 100,
+    "img": `${urlBase}sapato.png`,
+    "cat": 'R'
+},
+{
+    "nome": "Mesa",
+    "preco": 140,
+    "img": `${urlBase}mesa.png`,
+    "cat": 'M'
+},
+{
+    "nome": "Cadeira",
+    "preco": 170,
+    "img": `${urlBase}cadeira-de-escritorio.png`,
+    "cat": 'M'
+},
+{
+    "nome": "Saia",
+    "preco": 38.99,
+    "img": `${urlBase}saia.png`,
+    "cat": 'R'
+},
+{
+    "nome": "Calça jeans",
+    "preco": 80,
+    "img": `${urlBase}calca-jeans.png`,
+    "cat": 'R'
+},
+{
+    "nome": "Camisa",
+    "preco": 30,
+    "img": `${urlBase}camisa.png`,
+    "cat": 'R'
+},
+{
+    "nome": "Chapéu",
+    "preco": 30,
+    "img": `${urlBase}chapeu-de-palha.png`,
+    "cat": 'R'
+},
+{
+    "nome": "Mouse",
+    "preco": 20,
+    "img": `${urlBase}mouse.png`,
+    "cat": 'I'
+},
+{
+    "nome": "Teclado de música",
+    "preco": 60,
+    "img": `${urlBase}teclado-musica.png`,
+    "cat": 'I'
+},
+{
+    "nome": "Teclado",
+    "preco": 20,
+    "img": `${urlBase}teclado.png`,
+    "cat": 'I'
+},
+{
+    "nome": "Refrigerador",
+    "preco": 400,
+    "img": `${urlBase}refrigerador.png`,
+    "cat": 'E'
+},
+{
+    "nome": "Ventilador",
+    "preco": 80,
+    "img": `${urlBase}Ventilador.png`,
+    "cat": 'E'
+},
+{
+    "nome": "Esmalte",
+    "preco": 3.79,
+    "img": `${urlBase}esmalte.png`,
+    "cat": 'C'
+},
+{
+    "nome": "Poltrona",
+    "preco": 150,
+    "img": `${urlBase}poltrona.png`,
+    "cat": 'M'
+},
+];
