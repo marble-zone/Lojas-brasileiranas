1f49d54374ee7db7674ae011d617baac7e89eec1
FlautistaMacabro <vinicius.pelegrineli@unesp.br>
2021-06-06 22:50:29 -0300

    Modificando a regra do input de preço pra aceitar vígula

--- js/shooping/filterCategory.js
+++ js/shooping/filterCategory.js
@@ -103,13 +103,13 @@ function validate(evt) {
   
     // Handle paste
     if (theEvent.type === 'paste') {
-        key = event.clipboardData.getData('text/plain');
+        key = evt.clipboardData.getData('text/plain');
     } else {
     // Handle key press
         var key = theEvent.keyCode || theEvent.which;
         key = String.fromCharCode(key);
     }
-    var regex = /[0-9]|\./;
+    var regex = /[0-9]|\.|\,/;
     if( !regex.test(key) ) {
       theEvent.returnValue = false;
       if(theEvent.preventDefault) theEvent.preventDefault();
